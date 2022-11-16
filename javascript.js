6d3a7765fda4be56aa66afc7fd484412925fb14b
cleberson <movcleber@gmail.com>
2022-11-16 08:48:46 -0300

    exemplos em javascript

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const altura = 5;
+const comprimento = 7;
+const base = 2;
+const forma = 'triangulo';
+const forma2 = 'quadrado';
+
+
+const area = altura * comprimento; 
+const area2 = (altura * comprimento) / base;
+
+if (forma === 'triangulo') {
+    console.log(area);
+} else  {
+    console.log(area2);
+}
